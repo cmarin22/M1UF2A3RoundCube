ddeb99c64a545241f7c8a1cd193cc7f09a72b56b
cmarin22 <116750777+cmarin22@users.noreply.github.com>
2023-04-12 06:10:04 +0200

    Update roundcube.sh

--- roundcube.sh
+++ roundcube.sh
@@ -13,38 +13,48 @@ ROJOBK = '\e[41m'
 #Aquest condicional utilitza la comanda “whoami”, serveix per identificar l’usuari actual
 #Compara la variable si es == a “root” en cas afirmatiu escriu “Ets root.” i en cas negatiu et diu que no ho ets i surt de l’script
 if [ $(whoami) == "root" ]; then
-
-
         echo -e "${VERDE} Ets root. ${NORMAL} "
         # Color verd
-
-
 else
-
-
-        echo "${ROJO}No ets root. ${NORMAL} "
+        echo -e "${ROJO}No ets root. ${NORMAL} "
         # Color vermell
-        echo "${ROJOBK}No tens permisos per executar aquest script, només pots ser executat per l'usuari root.${NORMAL}"
+        echo -e "${ROJOBK}No tens permisos per executar aquest script, només pots ser executat per l'usuari root.${NORMAL}"
         # Fons vermell
         # Exit fa que sortim de l'script.
         exit
-
-
 fi
 
 
 #Instal.lació paquet Apache2
-if [ $(dpkg-query -W -f='${Status}' 'apache2' | grep -c "ok installed") -eq 0 ];then 
-        echo "Apache2 no està instal.lat" >/script/registre.txt
+if [ $(dpkg-query -W -f='${Status}' 'apache2' | grep -c "ok installed") -eq 0 ];then
+# Si no trobem Apache2, avisem que no està instal·lat
+        echo "Apache2 no està instal.lat" >>/script/registre.txt
+        echo "Apache2 no està instal.lat"
         apt-get -y install apache2 >/dev/null 2>&1
         if [ $? -eq 0 ];then
                 echo "Apache2 instal.lat correctament." >>/script/registre.txt
-                echo "Apache2 instal.lat correctament."
+                echo -e "${VERDE}Apache2 instal·lat correctament.${NORMAL}"
         else
-                echo "Apache2 instal.lat incorrectament."
+                echo -e "${ROJO}Apache2 instal.lat incorrectament.${NORMAL}"
+                exit
         fi
 else
-        echo "Apache2 està instal.lat"
+        echo "Apache2 ja està instal.lat"
+fi
+
+#Instal.lació paquet MariaDB-Server
+if [ $(dpkg-query -W -f='${Status}' 'mariadb-server' | grep -c "ok installed") -eq 0 ];then 
+        echo "MariaDB-Server no està instal.lat" >/script/registre.txt
+        apt-get -y install apache2 >/dev/null 2>&1
+        if [ $? -eq 0 ];then
+                echo "MariaDB-Server instal·lat correctament." >>/script/registre.txt
+                echo -e "${VERDE}MariaDB-Server instal·lat correctament.${NORMAL}"
+        else
+                echo -e "${ROJO}Apache2 instal.lat incorrectament.${NORMAL}"
+                exit
+        fi
+else
+        echo "MariaDB-Server ja està instal.lat"
 fi
 
 
