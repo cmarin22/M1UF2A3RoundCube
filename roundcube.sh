fea404981e9d1c5cb328bb9084ad294a756a108e
cmarin22 <116750777+cmarin22@users.noreply.github.com>
2023-04-12 06:22:23 +0200

    Update roundcube.sh

--- roundcube.sh
+++ roundcube.sh
@@ -3,20 +3,20 @@ clear
 # Amb netegem el terminal
 
 # Declaració dels diferents colors
-NORMAL = '\e[0m'
-ROJO = '\e[31m'
-VERDE = '\e[32m'
-ROJOBK = '\e[41m'
+NORMAL='\e[0m'
+ROJO='\e[31m'
+VERDE='\e[32m'
+ROJOBK='\e[41m'
  
-
+echo "SCRIPT AUTOMÀTIC PER INSTAL·LAR EL SERVIDOR ROUNDCUBE"
 #Comprovació de l’usuari
 #Aquest condicional utilitza la comanda “whoami”, serveix per identificar l’usuari actual
 #Compara la variable si es == a “root” en cas afirmatiu escriu “Ets root.” i en cas negatiu et diu que no ho ets i surt de l’script
 if [ $(whoami) == "root" ]; then
-        echo -e "${VERDE} Ets root. ${NORMAL} "
+        echo -e "${VERDE}Ets root.${NORMAL} "
         # Color verd
 else
-        echo -e "${ROJO}No ets root. ${NORMAL} "
+        echo -e "${ROJO}No ets root.${NORMAL} "
         # Color vermell
         echo -e "${ROJOBK}No tens permisos per executar aquest script, només pots ser executat per l'usuari root.${NORMAL}"
         # Fons vermell
@@ -44,7 +44,7 @@ fi
 
 #Instal.lació paquet MariaDB-Server
 if [ $(dpkg-query -W -f='${Status}' 'mariadb-server' | grep -c "ok installed") -eq 0 ];then 
-        echo "MariaDB-Server no està instal.lat" >/script/registre.txt
+        echo "MariaDB-Server no està instal.lat" >>/script/registre.txt
         apt-get -y install apache2 >/dev/null 2>&1
         if [ $? -eq 0 ];then
                 echo "MariaDB-Server instal·lat correctament." >>/script/registre.txt
